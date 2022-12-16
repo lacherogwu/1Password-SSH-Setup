abc88a7e9f12fee837964782d89694015b2b478f
lachero <asaf@utopia.tech>
2022-12-16 17:17:38 +0200

    added support for id_ed25519 private key type

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
 import Ask from 'https://deno.land/x/ask@1.0.6/mod.ts';
 
 const HOME_DIR = Deno.env.get('HOME');
-const PRIVATE_KEY_PATH = `${HOME_DIR}/Downloads/id_rsa`;
 const SSH_DIR = `${HOME_DIR}/.ssh`;
 
 const ask = new Ask();
@@ -28,8 +27,8 @@ await movePrivateKey(name);
 addToSSHConfig(host, name);
 
 async function movePrivateKey(fileName: string) {
-	await verifyPrivateKeyExists();
-	await Deno.rename(PRIVATE_KEY_PATH, `${SSH_DIR}/${fileName}.pem`);
+	const privateKeyPath = await getPrivateKeyPath();
+	await Deno.rename(privateKeyPath, `${SSH_DIR}/${fileName}.pem`);
 }
 
 async function addToSSHConfig(host: string, fileName: string) {
@@ -37,13 +36,23 @@ async function addToSSHConfig(host: string, fileName: string) {
 	await Deno.copyFile(`${SSH_DIR}/config`, `${SSH_DIR}/config-${fileSuffix}`);
 	const file = await Deno.open(`${SSH_DIR}/config-${fileSuffix}`, { append: true });
 	const encoder = new TextEncoder();
-	const data = encoder.encode(`\n\nHost ${host}\n  IdentityFile ~/.ssh/${fileName}.pem\n  IdentitiesOnly yes`);
+	const data = encoder.encode(`\n\nHost ${host} # ${fileName}\n  IdentityFile ~/.ssh/${fileName}.pem\n  IdentitiesOnly yes`);
 	await file.write(data);
 	file.close();
 	await Deno.copyFile(`${SSH_DIR}/config-${fileSuffix}`, `${SSH_DIR}/config`);
 	await Deno.remove(`${SSH_DIR}/config-${fileSuffix}`);
 }
 
-function verifyPrivateKeyExists() {
-	return Deno.stat(PRIVATE_KEY_PATH);
+async function getPrivateKeyPath() {
+	const nameList = ['id_rsa', 'id_ed25519'];
+	let output;
+	for (const name of nameList) {
+		const path = `${HOME_DIR}/Downloads/${name}`;
+		const exists = await Deno.stat(path).catch(() => false);
+		if (!exists) continue;
+		output = path;
+	}
+	if (!output) throw new Error('No private key found');
+
+	return output;
 }
